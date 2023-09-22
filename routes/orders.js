80b6a51503390524521557402dcd0888711ae695
wahid_9000 <wahidchowdhury9000@gmail.com>
2023-09-22 22:38:21 +0600

    search and sort api created

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Order = require('../models/ModelOrders');
+const Order = require("../models/ModelOrders");
 
-router.get('/', async(req, res) => {
-    try {
-        const orders = await Order.find();
-        res.send(orders)
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch Orders" });
+//get orders by search
+router.get("/search", async (req, res) => {
+  const searchQuery = req.query.search;
+  console.log(searchQuery);
+  try {
+    let searchResults;
+
+    // Checking if the searchQuery is a valid number
+    const searchQueryAsNumber = parseInt(searchQuery);
+    if (!isNaN(searchQueryAsNumber)) {
+      // performing exact search on order_id as number
+      searchResults = await Order.find({ order_id: searchQueryAsNumber });
+    } else {
+      // performing regex search on other fields
+      searchResults = await Order.find({
+        $or: [
+          { customer_name: { $regex: searchQuery, $options: "i" } },
+          { "product.product_name": { $regex: searchQuery, $options: "i" } },
+        ],
+      });
     }
-})
+
+    res.json(searchResults);
+  } catch (error) {
+    console.error("Error during search:", error);
+    res.status(500).json({ error: "An Error Occurred" });
+  }
+});
+
+//get all orders
+router.get("/all-orders", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ order_id: 1 });
+    res.send(orders);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Orders" });
+  }
+});
+
+// Get all orders sorted by order_date in ascending order
+router.get("/all-orders-asc", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ order_date: 1 });
+    res.send(orders);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Orders" });
+  }
+});
+
+// Get all orders sorted by order_date in descending order
+router.get("/all-orders-desc", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ order_date: -1 });
+    res.send(orders);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Orders" });
+  }
+});
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/ModelProducts');
 
-router.get('/', async(req, res) => {
+router.get('/all-products', async(req, res) => {
     try {
         const products = await Product.find();
         res.send(products);
-        console.log(products); 
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch Products" });
     }
